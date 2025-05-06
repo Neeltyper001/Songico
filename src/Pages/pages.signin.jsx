2c958c32f6adae9d079004b5b37092fd9f376001
Neelesh Joshi <neeleshjoshi001@gmail.com>
2025-05-06 21:34:30 +0530

    code cleaning

--- src/Pages/pages.signin.jsx
+++ src/Pages/pages.signin.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Box, Button, Container, Paper, Typography } from '@mui/material'
-import FormText from '../ui/FormText'
+import { Box, Button, Container,  Typography } from '@mui/material'
 import FormPassword from '../ui/FormPassword'
-import EmailIcon from '@mui/icons-material/Email';
 import GoogleButton from '../ui/GoogleButton/GoogleButton';
 import { useNavigate } from 'react-router-dom';
 import FormEmail from '../ui/FormEmail'
@@ -20,7 +18,7 @@ const Signin = () => {
         <Box sx={{position: 'relative', display: 'flex', flexDirection: 'row', gap: 2, justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
                     <Typography variant='h4' sx={{position:"absolute", top: 25, left: 20, color: '#3751FE' , fontSize: "34px", fontWeight: "bold" ,zIndex: 2}}>Sign In</Typography>
             <Container sx={{position:"relative",height: '500px' , display: 'flex', flexDirection: "column", justifyContent: 'center', alignItems: 'center', paddingX: 2 , background: 'transparent'}}>                
-                    <Box component={'form'} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'flexStart'}}>
+                    <Box component={'form'} onSubmit={handleSubmit} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'flexStart'}}>
                         <Box component={'img'} src="/assets/illustrations/signup.gif" sx={{display:{xs:"block",md: "none"}, height: "150px"}}/>  
                         <FormEmail setFormData={setFormData} setFormError={setFormError} variant={'standard'} label={'Email'}  value={''} />
                         <FormPassword setFormData={setFormData} setFormError={setFormError} variant={'standard'} label={'Password'} value={''} />                        
@@ -29,7 +27,7 @@ const Signin = () => {
                         <GoogleButton buttonTypeText={"Signin"}/>
                     </Box>                
                     </Box>
-                    <Typography sx={{marginY: 2, color: '#3751FE' , fontSize: "14px", fontWeight: "bold"}}>Don't have an account? <Button variant='text' sx={{textDecoration: 'underline'}} onClick={()=>{navigate('/signup')}}>Sign up</Button></Typography>
+                    <Typography sx={{marginY: 2, color: '#3751FE' , fontSize: "14px", fontWeight: "bold"}}>{`Don't have an account? `}<Button variant='text' sx={{textDecoration: 'underline'}} onClick={()=>{navigate('/signup')}}>Sign up</Button></Typography>
             </Container>
             <Container sx={{display: {xs:"none",md: "block"}}}>
                 <Box component={'img'} src="/assets/illustrations/signin.gif"/>                                     
--- src/routes/Approute.jsx
+++ src/routes/Approute.jsx
@@ -1,9 +1,6 @@
 import MainLayout from "../Layouts/layouts.main";
 import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
 import { createRoutesFromElements } from "react-router-dom";
-import React from 'react'
-import DefaultSection from "../Sections/DefaultSection";
-import SongSection from "../Sections/SongSection";
 import Signup from "../Pages/pages.singup";
 import Landing from "../Pages/pages.landing";
 import Signin from "../Pages/pages.signin";
