5a51a91ce8b72c7ac9010d2cbeb0adf18bad1264
Neelesh Joshi <neeleshjoshi001@gmail.com>
2025-05-11 18:12:08 +0530

    approute code cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import Header from './Components/Header'
-import MainSection from './Components/MainSection'
-import Footer from './Components/Footer'
 import AppRoute from './routes/Approute'
 
 function App() {  
   return (
     <>
-    <AppRoute />
-      {/* <Header />
-      <MainSection />
-      <Footer /> */}
+      <AppRoute />
     </>
   )
 }
