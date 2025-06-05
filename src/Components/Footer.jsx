3ea4707bca7e7db4c4618d1a0f9444f573b1f1b8
Neelesh Joshi <neeleshjoshi001@gmail.com>
2025-06-05 18:46:36 +0530

    updated footer year [fixed static year]

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,12 @@
+import { getYear } from "../utils/timeUtils";
+
 export default function Footer(){
     return(
         <footer className="bg-gray-50">
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
           <div className="mx-auto max-w-screen-xl">
             <p className="text-center">
-              Copyright &copy; 2023. All rights reserved.
+              {`Copyright &copy; ${getYear()}. All rights reserved.`}
             </p>
           </div>
         </div>
---
+++ src/utils/timeUtils.js
@@ -0,0 +1,5 @@
+export const getYear = ()=>{
+    const currentYear = new Date().getFullYear();
+    console.log(currentYear);
+    return currentYear;
+}
